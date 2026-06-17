307023c5566866e350dad37d32a9df4fc08a114e
Taro Ramacciotti <trama@login13.chn.perlmutter.nersc.gov>
2026-06-16 20:01:40 -0700

    finalizing project code att3

---
+++ s4bb/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,14 @@
+"""
+====
+s4bb
+====
+
+Cross-spectrum analysis, likelihood, forecasting
+
+"""
+
+from s4bb.util import mapind, specind, specgen, MapDef
+import s4bb.bandpass as bandpass
+import s4bb.bpcov as bpcov
+import s4bb.bpwf as bpwf
+import s4bb.spectra as spectra
